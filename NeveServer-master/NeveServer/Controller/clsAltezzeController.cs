186b253067f3664dde1a04d9c75d4bd42213ed94
Gionsi Anrango Ramos <acharig47@gmail.com>
2025-05-02 14:35:23 +0200

    added projects

---
+++ NeveServer-master/NeveServer/Controller/clsAltezzeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+using ADOSQLServer2017_ns;
+
+namespace NeveServer.Controller
+{
+    public class clsAltezzeController
+    {
+        ADOSQLServer2017 ado;
+        public clsAltezzeController(string dbName)
+        {
+            this.ado = new ADOSQLServer2017(dbName);
+        }
+    }
+}
